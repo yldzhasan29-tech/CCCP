1c17c5f9def833c7c4902032ba0e1d0dce3ae9d1
Oscar6789-ui <daviddrever54@gmail.com>
2026-09-05 17:30:01 +0500

    Create mockData.js

---
+++ js/mockData.js
@@ -0,0 +1,54 @@
+// Esta es tu lista de conductores de prueba para el Hackatón
+export const simulatedRides = [
+  {
+    id: 1,
+    driverName: "Александр",
+    age: 21,
+    car: "Toyota Camry",
+    rating: 4.9,
+    seatsAvailable: 3,
+    departureTime: "08:30",
+    origin: "Площадь 1905 года",
+    destination: "Новокольцовский кампус",
+    price: 120,
+    matchScore: 96,
+    emoji: "🚗",
+    note: "Почти идеальное совпадение по времени!",
+    music: "Techno",
+    chatty: false
+  },
+  {
+    id: 2,
+    driverName: "Роман",
+    age: 20,
+    car: "Kia Rio",
+    rating: 4.8,
+    seatsAvailable: 1,
+    departureTime: "08:35",
+    origin: "Главный корпус УрФУ",
+    destination: "Новокольцовский кампус",
+    price: 105,
+    matchScore: 91,
+    emoji: "☕",
+    note: "Маршрут совпадает, +5 минут к вашему времени",
+    music: "Rock",
+    chatty: true
+  },
+  {
+    id: 3,
+    driverName: "Дмитрий",
+    age: 22,
+    car: "Hyundai Solaris",
+    rating: 4.7,
+    seatsAvailable: 2,
+    departureTime: "08:40",
+    origin: "Втузгородок УрФУ",
+    destination: "Новокольцовский кампус",
+    price: 95,
+    matchScore: 84,
+    emoji: "💬",
+    note: "Самый дешёвый подходящий вариант",
+    music: "Pop",
+    chatty: true
+  }
+];
